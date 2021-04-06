9305f715dd0fc18340eb2f34d9f569ed5303800a
HenryMurdifulUlya <71810908+HenryMurdifulUlya@users.noreply.github.com>
2021-04-07 06:17:48 +0700

    Fix_Minggu3_Pertemuan3

--- minggu3_henry_web/app/Http/Controllers/ManagementUserController.php
+++ minggu3_henry_web/app/Http/Controllers/ManagementUserController.php
@@ -5,7 +5,10 @@ class ManagementUserController extends Controller
 {
     public function index(){
         //return "Halo ini adalah method index,dalam controller ManagementUser.";
-        return "Method ini nantinya akan digunakan untuk mengambil semua data user";
+        //return "Method ini nantinya akan digunakan untuk mengambil semua data user";
+        $nama ="Arvita Agus Kurniasari";
+        $pelajaran = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
+        return view('home', compact('nama','pelajaran'));
     }
     public function create(){
         return "Method ini nantinya akan digunakan untuk menampilkan form untuk menambah data user";
